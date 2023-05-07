02b7d04551deff572fe4b13b52a15fcfe6ada785
Still190494 <100068017+Still190494@users.noreply.github.com>
2023-05-07 16:31:10 +0300

    Lesson 5 (#5)
    * lesson_5
    
    * test
    
    * lesson_5_fin

--- data_client.py
+++ data_client.py
@@ -1,18 +1,35 @@
 from socket import *
-
+import json
 import sys
 from utils import get_msg, send_msg, msg_to_server
+import logging
+import logs.client_log_config
 
+
+
+logger = logging.getLogger('client')
 
 def main():
-    my_address = sys.argv[1]
-    my_port = int(sys.argv[2])
+    try:
+        my_address = sys.argv[1]
+        my_port = int(sys.argv[2])
+        if my_port < 1024 or my_port > 65535:
+            raise ValueError
+    except IndexError:
+        my_address = '127.0.0.1'
+        my_port = 7777
+    except ValueError:
+        logger.critical(f'Указан не верный порт!!! Порт не может быть меньше "1024" или больше "65535"')
+        sys.exit(1)
     s = socket(AF_INET, SOCK_STREAM) # Создать сокет TCP
     s.connect((my_address, my_port)) # Соединиться с сервером
     msg_to_s = msg_to_server()
     send_msg(s, msg_to_s)
-    answer = get_msg(s)
-    print('Сообщение от сервера: ', answer)
+    try:
+        answer = get_msg(s)
+        logger.info(f'Сообщение от сервера: {answer}')
+    except (ValueError, json.JSONDecodeError):
+        logger.critical(f'Сообщение в неправильном формате')
     s.close()
 
 
--- data_server.py
+++ data_server.py
@@ -2,11 +2,34 @@
 from socket import *
 import sys
 from utils import get_msg, send_msg, msg_to_client
+import json
+import logging
+import logs.server_log_config
 
+logger = logging.getLogger('server')
 
 def main():
-    my_port = int(sys.argv[sys.argv.index('-p') + 1])
-    my_address = sys.argv[sys.argv.index('-a') + 1]
+    try:
+        if '-p' in sys.argv:
+            my_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            my_port = 7777
+        if my_port < 1024 or my_port > 65535:
+            raise ValueError
+    except IndexError:
+        logger.critical(f'Не верно указан порт сервера')
+        sys.exit(1)
+    except ValueError:
+        logger.critical(f'Порт не может быть меньше "1024" или больше "65535"')
+        sys.exit(1)
+    try:
+        if '-a' in sys.argv:
+            my_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            my_address = '127.0.0.1'
+    except IndexError:
+        logger.critical(f'Не верно указан адресс сервера')
+        sys.exit(1)
     s = socket(AF_INET, SOCK_STREAM) # Создает сокет TCP
     s.bind((my_address, my_port))
     s.listen(5) # Переходит в режим ожидания запросов;
@@ -14,11 +37,15 @@ def main():
                 # 5 запросов.
     while True:
         client, addr = s.accept()
-        answer = get_msg(client)
-        print("Сообщение клиента", answer)
-        response_msg = msg_to_client()
-        send_msg(client, response_msg)
-        client.close()
+        try:
+            answer = get_msg(client)
+            logger.info(f'Сообщение клиента {answer}')
+            response_msg = msg_to_client()
+            send_msg(client, response_msg)
+            client.close()
+        except (ValueError, json.JSONDecodeError):
+            logger.critical(f'Сообщение в неправильном формате')
+            client.close()
 
 
 if __name__ == '__main__':
