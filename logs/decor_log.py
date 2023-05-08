6f35b699c5d6ad12cb61918c46b39621e699f1d1
Still190494 <Still190494@ya.ru>
2023-05-08 14:04:03 +0300

    lesson_6

--- logs/decor_log.py
+++ logs/decor_log.py
@@ -25,13 +25,3 @@ def log(func):
         return log_save
     else:
         return func
-
-# def log(func_log):
-#     def func_log(*args, **kwargs):
-#         r = func_log(*args, **kwargs)
-#         logger.info(f'Была вызвана функция {func_log.__name__} c аргументами {args}, {kwargs}. '
-#                     f'Вызов из модуля {func_log.__module__}. Вызов из'
-#                     f' функции {traceback.format_stack()[0].strip().split()[-1]}.'
-#                     f'Вызов из функции {inspect.stack()[1][3]}')
-#         return r
-#     return func_log
