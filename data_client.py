bde548aaf55fb6bfa64d38d1c854d9ae88295526
Still190494 <Still190494@ya.ru>
2023-05-28 23:37:47 +0300

    lesson_10

--- data_client.py
+++ data_client.py
@@ -40,9 +40,6 @@ class Client():
         """
         Функция запрашивает кому отправить сообщение и само сообщение,
         и отправляет полученные данные на сервер
-        :param sock:
-        :param account_name:
-        :return:
         """
         to_user = input('Введите получателя сообщения: ')
         message = input('Введите сообщение для отправки: ')
@@ -148,36 +145,7 @@ class Client():
                 if receiver.is_alive() and user_interface.is_alive():
                     continue
                 break
-# @log
-# def create_presence(account_name):
-#     """Функция генерирует запрос о присутствии клиента"""
-#     out = {
-#         "action": "presence",
-#         "time": time.time(),
-#         "user": account_name
-#     }
-#     logger.info(f'Сформировано сообщение для пользователя {account_name}')
-#     return out
 
-# @log
-# def message_from_server(sock, my_username):
-#     """Функция - обработчик сообщений других пользователей, поступающих с сервера"""
-#     while True:
-#         try:
-#             message = get_msg(sock)
-#             if 'action' in message and message['action'] == 'message' and \
-#                     'from' in message and 'to_user' in message \
-#                     and 'msg_text' in message and message['to_user'] == my_username:
-#                 print(f'\nПолучено сообщение от пользователя {message["from"]}:'
-#                       f'\n{message["msg_text"]}')
-#                 logger.info(f'Получено сообщение от пользователя {message["from"]}:'
-#                             f'\n{message["msg_text"]}')
-#             else:
-#                 logger.error(f'Получено некорректное сообщение с сервера: {message}')
-#         except (OSError, ConnectionError, ConnectionAbortedError,
-#                 ConnectionResetError, json.JSONDecodeError):
-#             logger.critical(f'Потеряно соединение с сервером.')
-#             break
 
 @log
 def create_arg_parser():
@@ -187,102 +155,6 @@ def create_arg_parser():
     parser.add_argument('-n', '--name', default=None, nargs='?')
     return parser
 
-# @log
-# def create_message(sock, account_name='admin'):
-#     """
-#     Функция запрашивает кому отправить сообщение и само сообщение,
-#     и отправляет полученные данные на сервер
-#     :param sock:
-#     :param account_name:
-#     :return:
-#     """
-#     to_user = input('Введите получателя сообщения: ')
-#     message = input('Введите сообщение для отправки: ')
-#     message_dict = {
-#         'action': 'message',
-#         'from': account_name,
-#         'to_user': to_user,
-#         'time': time.time(),
-#         'msg_text': message
-#     }
-#     logger.debug(f'Сформирован словарь сообщения: {message_dict}')
-#     try:
-#         send_msg(sock, message_dict)
-#         logger.info(f'Отправлено сообщение для пользователя {to_user}')
-#     except:
-#         logger.critical('Потеряно соединение с сервером.')
-#         sys.exit(1)
-
-# @log
-# def user_interactive(sock, username):
-#     while True:
-#         command = input('Введите команду: message(написаль сообщение) или exit(выйти) ')
-#         if command == 'message':
-#             create_message(sock, username)
-#         elif command == 'exit':
-#             send_msg(sock, create_exit_message(username))
-#             print('Завершение соединения.')
-#             logger.info('Завершение работы по команде пользователя.')
-#             # Задержка неоходима, чтобы успело уйти сообщение о выходе
-#             time.sleep(0.5)
-#             break
-#         else:
-#             print('Команда не распознана')
-
-
-# def main_client():
-#     parser = create_arg_parser()
-#     namespace = parser.parse_args(sys.argv[1:])
-#     try:
-#         my_address = namespace.addr
-#         my_port = namespace.port
-#         client_name = namespace.name
-#         if my_port < 1024 or my_port > 65535:
-#             raise ValueError
-#     except IndexError:
-#         my_address = '127.0.0.1'
-#         my_port = 7777
-#     except ValueError:
-#         logger.critical(f'Указан не верный порт!!! Порт не может быть меньше "1024" или больше "65535"')
-#         sys.exit(1)
-#     if not client_name:
-#         client_name = input('Введите имя пользователя: ')
-#     try:
-#         s = socket(AF_INET, SOCK_STREAM) # Создать сокет TCP
-#         s.connect((my_address, my_port)) # Соединиться с сервером
-#         send_msg(s, create_presence(client_name))
-#         answer = get_msg(s)
-#         logger.info(f'Установлено соединение с сервером. Ответ сервера: {answer}')
-#         print(f'Установлено соединение с сервером.')
-#         print(f'Запущен клиент с парамертами: адрес сервера: {my_address}, '
-#         f'порт: {my_port}, имя пользователя: {client_name}')
-#         logger.info(
-#         f'Запущен клиент с парамертами: адрес сервера: {my_address}, '
-#         f'порт: {my_port}, имя пользователя: {client_name}')
-#     except json.JSONDecodeError:
-#         logger.error('Не удалось декодировать полученную Json строку.')
-#         sys.exit(1)
-#     except (ConnectionRefusedError, ConnectionError):
-#         logger.critical(
-#             f'Не удалось подключиться к серверу {my_address}:{my_port}, '
-#             f'конечный компьютер отверг запрос на подключение.')
-#         sys.exit(1)
-#     else:
-#     # Если соединение с сервером установлено корректно,
-#     # запускаем клиенский процесс приёма сообщний
-#         receiver = threading.Thread(target=message_from_server, args=(s, client_name))
-#         receiver.daemon = True
-#         receiver.start()
-#     # затем запускаем отправку сообщений и взаимодействие с пользователем.
-#         user_interface = threading.Thread(target=user_interactive, args=(s, client_name))
-#         user_interface.daemon = True
-#         user_interface.start()
-#         logger.debug('Запущены процессы')
-#         while True:
-#             time.sleep(1)
-#             if receiver.is_alive() and user_interface.is_alive():
-#                 continue
-#             break
 
 def main():
     parser = create_arg_parser()
--- data_server.py
+++ data_server.py
@@ -1,6 +1,6 @@
 # Программа сервера для получения приветствия от клиента и отправки ответа
 import argparse
-from socket import *
+import socket
 import sys
 from utils import get_msg, send_msg, msg_to_client
 from logs.decor_log import log
@@ -9,65 +9,12 @@ import json
 import logging
 import logs.server_log_config
 import select
-
+from metaclasses import ServerVerifier
 
 sys.setrecursionlimit(10000)
 logger = logging.getLogger('server')
 
-
-# @log
-# def process_client_message(message, messages_list, client, clients, names):
-#     """
-#     Обработчик сообщений от клиентов, принимает словарь - сообщение от клиента,
-#     проверяет корректность, отправляет словарь-ответ в случае необходимости.
-#     :param message:
-#     :param messages_list:
-#     :param client:
-#     :param clients:
-#     :param names:
-#     :return:
-#     """
-#     logger.debug(f'Разбор сообщения от клиента : {message}')
-#     # Если это сообщение о присутствии, принимаем и отвечаем
-#     if "action" in message and message["action"] == "presence" and \
-#             "time" in message and "user" in message:
-#         # Если такой пользователь ещё не зарегистрирован,
-#         # регистрируем, иначе отправляем ответ и завершаем соединение.
-#         if message["user"] not in names.keys():
-#             names[message["user"]] = client
-#             resp_ok = msg_to_client()
-#             send_msg(client, resp_ok)
-#         else:
-#             response = {"response": 400,
-#                     "error": None
-#                     }
-#             response["error"] = 'Имя пользователя уже занято.'
-#             send_msg(client, response)
-#             clients.remove(client)
-#             client.close()
-#         return   
-#     # Если это сообщение, то добавляем его в очередь сообщений.
-#     # Ответ не требуется.
-#     elif "action" in message and message["action"] == 'message' and \
-#             "to_user" in message and "time" in message \
-#             and "from" in message and "msg_text" in message:
-#         messages_list.append(message)
-#         return
-#     # Если клиент выходит
-#     elif "action" in message and message["action"] == "exit" and "user" in message:
-#         clients.remove(names[message["user"]])
-#         names[message["user"]].close()
-#         del names[message["user"]]
-#         return
-#     # Иначе отдаём Bad request
-#     else:
-#         response = {"response": 400,
-#                     "error": None
-#                     }
-#         response["error"] = 'Запрос некорректен.'
-#         send_msg(client, response)
-#         return
-    
+   
 @log
 def create_arg_parser():
     parser = argparse.ArgumentParser()
@@ -75,29 +22,7 @@ def create_arg_parser():
     parser.add_argument('-a', default='127.0.0.1', nargs='?')
     return parser
 
-
-# @log
-# def process_message(message, names, listen_socks):
-#     """
-#     Функция адресной отправки сообщения определённому клиенту. Принимает словарь сообщение,
-#     список зарегистрированых пользователей и слушающие сокеты. Ничего не возвращает.
-#     :param message:
-#     :param names:
-#     :param listen_socks:
-#     :return:
-#     """
-#     if message["to_user"] in names and names[message["to_user"]] in listen_socks:
-#         send_msg(names[message["to_user"]], message)
-#         logger.info(f'Отправлено сообщение пользователю {message["to_user"]} '
-#                     f'от пользователя {message["from"]}.')
-#     elif message["to_user"] in names and names[message["to_user"]] not in listen_socks:
-#         raise ConnectionError
-#     else:
-#         logger.error(
-#             f'Пользователь {message["to_user"]} не зарегистрирован на сервере, '
-#             f'отправка сообщения невозможна.')
-
-class Server():
+class Server(metaclass=ServerVerifier):
     my_port = DescriptPort()
     def __init__(self, my_address, my_port):
         self.my_address = my_address
@@ -105,6 +30,7 @@ class Server():
         self.clients = []
         self.messages = []
         self.names = dict()   
+
 
     def init_socket(self):
         try:
@@ -115,10 +41,17 @@ class Server():
         except IndexError:
             logger.critical(f'Не верно указан адресс сервера')
             sys.exit(1)
-        self.s = socket(AF_INET, SOCK_STREAM) # Создает сокет TCP
-        self.s.bind((self.my_address, self.my_port))
-        self.s.settimeout(0.5)
-        self.s.listen()
+        my_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        my_server.bind((self.my_address, self.my_port))
+        my_server.settimeout(0.5)
+
+        # Начинаем слушать сокет.
+        self.my_server = my_server
+        self.my_server.listen()
+        # self.s = socket(AF_INET, SOCK_STREAM) # Создает сокет TCP
+        # self.s.bind((self.my_address, self.my_port))
+        # self.s.settimeout(0.5)
+        # self.s.listen()
 
 
     @log
@@ -126,10 +59,6 @@ class Server():
         """
         Функция адресной отправки сообщения определённому клиенту. Принимает словарь сообщение,
         список зарегистрированых пользователей и слушающие сокеты. Ничего не возвращает.
-        :param message:
-        :param names:
-        :param listen_socks:
-        :return:
         """
         if message["to_user"] in self.names and self.names[message["to_user"]] in listen_socks:
             send_msg(self.names[message["to_user"]], message)
@@ -193,18 +122,15 @@ class Server():
 
     def main_server(self):
         self.init_socket()
-
-
         while True:
             try:
-                client, addr = self.s.accept()
+                client, addr = self.my_server.accept()
             except OSError:
                 pass
             else:
                 logger.info(f'Установлено соедение с {addr}')
                 print("Получен запрос на соединение от %s" % str(addr))
                 self.clients.append(client)
-
             recv_list = []
             send_list = []
             err_list = []
@@ -232,77 +158,6 @@ class Server():
     print('Запущен сервер')
 
 
-
-
-
-
-# def main_server():
-#     parser = create_arg_parser()
-#     namespace = parser.parse_args(sys.argv[1:])
-#     my_address = namespace.a
-#     my_port = namespace.p
-#     clients = []
-#     messages = []
-#     names = dict()
-#     try:
-#         if my_port < 1024 or my_port > 65535:
-#             raise ValueError
-#     except IndexError:
-#         logger.critical(f'Не верно указан порт сервера')
-#         sys.exit(1)
-#     except ValueError:
-#         logger.critical(f'Порт не может быть меньше "1024" или больше "65535"')
-#         sys.exit(1)
-#     try:
-#         if '-a' in sys.argv:
-#             my_address = sys.argv[sys.argv.index('-a') + 1]
-#         else:
-#             my_address = '127.0.0.1'
-#     except IndexError:
-#         logger.critical(f'Не верно указан адресс сервера')
-#         sys.exit(1)
-#     s = socket(AF_INET, SOCK_STREAM) # Создает сокет TCP
-#     s.bind((my_address, my_port))
-#     s.settimeout(0.5)
-#     s.listen(5) # Переходит в режим ожидания запросов;
-#                 # Одновременно обслуживает не более
-#                 # 5 запросов.
-#     while True:
-#         try:
-#             client, addr = s.accept()
-#         except OSError:
-#             pass
-#         else:
-#             logger.info(f'Установлено соедение с {addr}')
-#             print("Получен запрос на соединение от %s" % str(addr))
-#             clients.append(client)
-
-#         recv_list = []
-#         send_list = []
-#         err_list = []
-#         try:
-#             if clients:
-#                 recv_list, send_list, err_list = select.select(clients, clients, [], 0)
-#         except OSError:
-#             pass
-#         if recv_list:
-#             for client_with_message in recv_list:
-#                 try:
-#                     process_client_message(get_msg(client_with_message),
-#                                            messages, client_with_message, clients, names)
-#                 except:
-#                     logger.info(f'Клиент {client_with_message.getpeername()} '
-#                                 f'отключился от сервера.')
-#                     clients.remove(client_with_message)
-#         for i in messages:
-#             try:
-#                 process_message(i, names, send_list)
-#             except Exception:
-#                 logger.info(f'Связь с клиентом с именем {i["to_user"]} была потеряна')
-#                 clients.remove(names[i["to_user"]])
-#                 del names[i["to_user"]]
-#         messages.clear()
-# print('Запущен сервер')
 def main():
     parser = create_arg_parser()
     namespace = parser.parse_args(sys.argv[1:])
@@ -310,7 +165,6 @@ def main():
     my_port = namespace.p
     my_server = Server(my_address, my_port)
     my_server.main_server()
-
 
 
 if __name__ == '__main__':
