5bee92449084c0888125ae6a48cf5ccca8931337
Still190494 <Still190494@ya.ru>
2023-06-16 00:16:06 +0300

    lesson_15_2

--- launcher.py
+++ launcher.py
@@ -1,7 +1,6 @@
 import subprocess
 
 process = int(input("Введите нужное колличество запускаемых приложений: "))
-subprocess.Popen('python data_server.py -p 7777 -a localhost',creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python data_server.py -p 7777 -a localhost', creationflags=subprocess.CREATE_NEW_CONSOLE)
 for num in range(1, process + 1):
     subprocess.Popen(f'python data_client.py localhost 7777 -n test_{num}', creationflags=subprocess.CREATE_NEW_CONSOLE)
-
