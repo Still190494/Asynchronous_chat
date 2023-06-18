398b5e874cec5e28ae5d814d935fd9e89109184c
Still190494 <Still190494@ya.ru>
2023-06-18 22:06:19 +0300

    ok

--- pack_server/setup.py
+++ pack_server/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="client_chat_pyqt_still190494",
+setup(name="server_chat_pyqt_still190494",
       version="0.1",
-      description="client_chat_pyqt_still190494",
+      description="server_chat_pyqt_still190494",
       author="Artem",
       author_email="artem@yandex.ru",
       packages=find_packages(),
